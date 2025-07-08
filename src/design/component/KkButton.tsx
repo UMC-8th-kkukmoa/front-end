ef8c84cdfb63f4b081c3013977443a5c375e456e
onebone <jyc00410@gmail.com>
2025-07-09 03:24:13 +0900

    feat: add KkButton component

---
+++ src/design/component/KkButton.tsx
@@ -0,0 +1,72 @@
+import {StyleProp, StyleSheet, Text, TouchableOpacity, View, ViewStyle} from "react-native";
+import {JSX} from "react";
+import colors from "../colors.ts";
+
+export type KkButtonType = 'primary' | 'secondary';
+export type KkButtonSize = 'large' | 'small';
+
+export interface KkButtonProps {
+	style?: StyleProp<ViewStyle>,
+	label: string,
+	type: KkButtonType,
+	size: KkButtonSize,
+	onPress: () => void,
+	left?: JSX.Element,
+}
+
+export function KkButton(
+	{ style, label, type, size, onPress, left }: KkButtonProps,
+) {
+  return (
+	  <TouchableOpacity
+		  style={[
+			  styles.buttonStyles,
+			  type === 'primary' ? styles.variantPrimary : styles.variantSecondary,
+			  size === 'large' ? styles.sizeLarge : styles.sizeSmall,
+			  style
+		  ]}
+		  accessibilityRole="button"
+		  accessible={true}
+		  onPress={onPress}>
+		  <View>
+			  {left ? left : <></>}
+
+			  <Text style={[
+				  styles.buttonTextStyles,
+				  type === 'primary' ? styles.variantPrimaryText : styles.variantSecondaryText,
+			  ]}>{label}</Text>
+		  </View>
+	  </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+	buttonStyles: {
+		borderRadius: 30,
+		shadowColor: '#6C313126',
+		display: 'flex',
+		alignItems: 'center',
+	},
+	buttonTextStyles: {
+		fontSize: 15,
+		fontWeight: '600',
+	},
+	variantPrimary: {
+		backgroundColor: colors.light.main,
+	},
+	variantPrimaryText: {
+		color: colors.light.white,
+	},
+	variantSecondary: {
+		backgroundColor: colors.light.white,
+	},
+	variantSecondaryText: {
+		color: colors.light.black,
+	},
+	sizeLarge: {
+		paddingVertical: 17,
+	},
+	sizeSmall: {
+		paddingVertical: 13.5,
+	},
+})
