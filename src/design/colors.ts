a2d5bd2c021b294c7cb914432d9c507eca6295e9
chae1125 <133013991+chae1125@users.noreply.github.com>
2025-07-16 02:44:43 +0900

    스탬프 목록 & 스탬프 수집 완료 모달 [#4] (#7)
    * chore: add font assets for Pretendard
    
    * feat: add CustomDropdown component
    
    * feat: add Header component
    
    * feat: add StampBoard component and stamp image asset
    
    * feat: add StampCompleteModal component
    
    * feat: add StampList screen
    
    * refactor: reorganize file structure and rename components
    
    * style: resize Kkdropdown component

--- src/design/colors.ts
+++ src/design/colors.ts
@@ -1,13 +1,13 @@
 const lightColors = {
-	main: '#FF8246',
-	sub: '#FFA275',
-	white: '#FFFFFF',
-	gray1_35: '#DCDCDC59',
-	gray1: '#DCDCDC',
-	gray2: '#888888',
-	black: '#2B2B2B'
-}
+  main: '#FF8246',
+  sub: '#FFA275',
+  white: '#FFFFFF',
+  gray1_35: '#DCDCDC59',
+  gray1: '#DCDCDC',
+  gray2: '#888888',
+  black: '#2B2B2B',
+};
 
 export default {
-	light: lightColors
+  light: lightColors,
 };
