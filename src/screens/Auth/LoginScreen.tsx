238b20eb3a7f333f80e298d1602c4e1d21f202d5
rladmsgki <rladmsgki@naver.com>
2025-07-24 20:23:40 +0900

    style: 로그 인 선택 버튼 그림자 추가

--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -96,6 +96,7 @@ export default function LoginScreen() {
           type={isValid ? 'primary' : 'disabled'}
           size="large"
           onPress={isValid ? handleLogin : () => {}}
+          shadow
         />
       </View>
 
