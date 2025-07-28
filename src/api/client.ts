ce6e215437fd6f1fe177a5946efdf2fdaa044315
onebone <jyc00410@gmail.com>
2025-07-29 03:44:49 +0900

    feat: add bearer from the secure storage

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,27 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
+import { getGenericPassword } from 'react-native-keychain';
 
 const apiClient = axios.create({
   baseURL: process.env.EXPO_PUBLIC_BASE_URL,
+});
+
+apiClient.interceptors.request.use(async (config) => {
+  const newConfig = { ...config };
+  // react-native-keychain은 RN에서만 사용 가능함
+  if (Platform.OS !== 'web') {
+    try {
+      const credential = await getGenericPassword();
+      if (credential) {
+        newConfig.headers.Authorization = `Bearer ${credential.password}`;
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Failed to get credential', error);
+    }
+  }
+
+  return newConfig;
 });
 
 export default apiClient;
