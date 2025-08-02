2d6b5d48b092346d51a24e0cf43e3855a0209a7c
Andy Yoonchul Jeong <jyc00410@gmail.com>
2025-08-02 13:32:27 +0900

    네트워크 요청을 위한 라이브러리 추가 (#28)
    * build: add axios, tanstack query for network access
    
    * feat: add bearer from the secure storage
    
    * feat: add check for EXPO_PUBLIC_BASE_URL
    
    * feat: add timeout to axios

---
+++ src/api/client.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { Platform } from 'react-native';
+import { getGenericPassword } from 'react-native-keychain';
+
+const baseUrl = process.env.EXPO_PUBLIC_BASE_URL;
+if (!baseUrl) {
+  throw new Error('EXPO_PUBLIC_BASE_URL is not defined.');
+}
+
+const apiClient = axios.create({
+  baseURL: baseUrl,
+  timeout: 10000,
+});
+
+apiClient.interceptors.request.use(async (config) => {
+  const newConfig = { ...config };
+  // react-native-keychain은 RN에서만 사용 가능함
+  if (Platform.OS !== 'web') {
+    try {
+      const credential = await getGenericPassword();
+      if (credential && credential.password) {
+        newConfig.headers.Authorization = `Bearer ${credential.password}`;
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Failed to get credential', error);
+    }
+  }
+
+  return newConfig;
+});
+
+export default apiClient;
