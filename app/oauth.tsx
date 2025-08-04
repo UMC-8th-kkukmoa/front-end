613d071e7bb553172c11dd4774061b15a0f436d6
rladmsgki <rladmsgki@naver.com>
2025-08-04 16:38:57 +0900

    feat: 카카오 로그인 후 리다이렉션을 위한 설정 추가

---
+++ app/oauth.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'expo-router';
+import * as Linking from 'expo-linking';
+import { View, ActivityIndicator } from 'react-native';
+
+export default function OAuthRedirectScreen() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRedirect = async () => {
+      const initialUrl = await Linking.getInitialURL();
+      console.log('[OAuth Redirect] initialUrl:', initialUrl);
+
+      router.replace('/(tabs)');
+    };
+
+    handleRedirect();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+}
