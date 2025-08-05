a59a675ed4aa5bb9cae3b3e65c81cec0182174da
rladmsgki <rladmsgki@naver.com>
2025-08-05 16:06:09 +0900

    style: 스타일 코드 분리

---
+++ src/screens/myGiftCard/MyGiftCardScreen.style.ts
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../design/colors';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cardContainer: {
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingTop: 15,
+  },
+  card: {
+    backgroundColor: '#DCDCDC59',
+    borderRadius: 12,
+    marginVertical: 12,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginHorizontal: 11,
+    marginTop: 15,
+    gap: 15,
+  },
+  daysLeft: {
+    fontSize: 14,
+    color: '#888',
+  },
+  statusBadge: {
+    paddingHorizontal: 11,
+    paddingVertical: 5,
+    borderRadius: 16,
+    elevation: 4,
+  },
+  unused: {
+    backgroundColor: colors.light.gray1,
+  },
+  used: {
+    backgroundColor: colors.light.sub,
+  },
+  statusText: {
+    color: colors.light.white,
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  image: {
+    width: 282,
+    height: 172,
+    borderRadius: 8,
+    marginTop: 14,
+    marginBottom: 15,
+    alignSelf: 'center',
+  },
+});
