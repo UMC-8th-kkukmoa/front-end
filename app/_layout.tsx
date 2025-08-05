5e2413206f1589369af6a81dfab425a08a9f6068
Andy Yoonchul Jeong <jyc00410@gmail.com>
2025-08-05 21:31:14 +0900

    Protected Route 적용 및 로그인 시 메인으로 이동 (#51)
    * feat: implement protected route
    
    * fix: no header is default for screens
    
    * fix: check credentials.password
    
    * fix: do not wait for the result of invalidateQueries

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,49 @@
-import { Slot } from 'expo-router';
 import React from 'react';
+import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ActivityIndicator, View } from 'react-native';
+import useAuth from '../src/hooks/useAuth';
 
 const queryClient = new QueryClient();
+
+function AppLayout() {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated === null) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={isAuthenticated}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="giftCard/GiftCardList" />
+        <Stack.Screen name="giftCard/GiftCardPurchase" />
+        <Stack.Screen name="giftCard/GiftCardDetail/[id]" />
+        <Stack.Screen name="myCoupon/MyCouponList" />
+        <Stack.Screen name="stamp/StampList" />
+        <Stack.Screen name="store/[id]" />
+        <Stack.Screen name="store/search" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isAuthenticated}>
+        <Stack.Screen name="auth/LoginChoiceScreen" />
+        <Stack.Screen name="oauth" />
+      </Stack.Protected>
+    </Stack>
+  );
+}
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <Slot />
+        <AppLayout />
       </GestureHandlerRootView>
     </QueryClientProvider>
   );
