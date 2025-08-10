10cd86d96c1db8e0f60b10546ca072ec517d9198
rladmsgki <rladmsgki@naver.com>
2025-08-10 22:01:52 +0900

    feat: 토큰 재발급

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
-import { getGenericPassword } from 'react-native-keychain';
+import { getGenericPassword, setGenericPassword } from 'react-native-keychain';
+import reissueTokens from './reissueTokens';
 
 const baseUrl = process.env.EXPO_PUBLIC_BASE_URL;
 if (!baseUrl) {
@@ -29,5 +30,38 @@ apiClient.interceptors.request.use(async (config) => {
 
   return newConfig;
 });
+
+// 401 Unauthorized 발생 시 토큰 재발급 후 재요청 처리
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // eslint-disable-next-line no-underscore-dangle
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      // eslint-disable-next-line no-underscore-dangle
+      originalRequest._retry = true;
+
+      const newTokens = await reissueTokens();
+
+      if (newTokens) {
+        try {
+          await setGenericPassword('accessToken', newTokens.accessToken, {
+            service: 'com.kkukmoa.accessToken',
+          });
+
+          // 요청 헤더에 새 accessToken 설정
+          originalRequest.headers.Authorization = `Bearer ${newTokens.accessToken}`;
+
+          return await apiClient(originalRequest);
+        } catch (e) {
+          console.error('Failed to update access token in Keychain', e);
+          return Promise.reject(error);
+        }
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default apiClient;
--- src/api/reissueTokens.ts
+++ src/api/reissueTokens.ts
@@ -1,22 +1,23 @@
 import * as Keychain from 'react-native-keychain';
-import apiClient from './client';
+import axios from 'axios';
 import { saveTokens } from '../utils/tokenStorage';
+
+const baseUrl = process.env.EXPO_PUBLIC_BASE_URL;
+if (!baseUrl) throw new Error('EXPO_PUBLIC_BASE_URL is not defined.');
 
 const reissueTokens = async () => {
   try {
     const refreshTokenCreds = await Keychain.getGenericPassword({
       service: 'com.kkukmoa.refreshToken',
     });
-
     if (!refreshTokenCreds) {
       console.warn('No refresh token found');
       return null;
     }
-
     const refreshToken = refreshTokenCreds.password;
 
-    const { data } = await apiClient.post(
-      '/v1/users/reissue',
+    const { data } = await axios.post(
+      `${baseUrl}/v1/users/reissue`,
       {},
       { headers: { Authorization: `Bearer ${refreshToken}` } },
     );
