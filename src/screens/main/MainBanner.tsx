e849a5e95825fea8aec52accf566d7e8e1ca197f
rladmsgki <152863626+rladmsgki@users.noreply.github.com>
2025-08-14 23:05:03 +0900

    배너 컴포넌트 [#82] (#87)
    * feat: 배너 컴포넌트 구현
    
    * feat: 배너 이미지 제거
    
    * feat: 가게 카드 비었을 떄 구현
    
    * style: gap으로 변경
    
    * feat: 이미지 제거

---
+++ src/screens/main/MainBanner.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
+import BannerImage1 from '../../assets/images/banner.svg';
+import colors from '../../design/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 12,
+    position: 'relative',
+    height: 121,
+    marginBottom: 27,
+  },
+  pagination: {
+    position: 'absolute',
+    bottom: 10,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1000,
+    gap: 4,
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: colors.light.white,
+    opacity: 0.5,
+  },
+  activeDot: {
+    borderRadius: 4,
+    backgroundColor: colors.light.white,
+    opacity: 1,
+  },
+});
+
+const banners = [{ id: 'banner1', component: BannerImage1 }];
+
+function MainBanner() {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Swiper
+        autoplay
+        autoplayTimeout={4}
+        loop
+        showsPagination={false}
+        onIndexChanged={setIndex}
+        scrollEnabled
+      >
+        {banners.map(({ id, component: BannerComponent }) => (
+          <BannerComponent key={id} width="100%" height={121} />
+        ))}
+      </Swiper>
+
+      {/* 커스텀 dot */}
+      <View style={styles.pagination} pointerEvents="none">
+        {banners.map(({ id }, idx) => (
+          <View key={id} style={[styles.dot, idx === index ? styles.activeDot : null]} />
+        ))}
+      </View>
+    </View>
+  );
+}
+
+export default MainBanner;
