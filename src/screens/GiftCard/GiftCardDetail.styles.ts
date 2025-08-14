0b02678679698cea33b33ac284a842ba29246bc8
rladmsgki <152863626+rladmsgki@users.noreply.github.com>
2025-08-14 22:10:49 +0900

    내 금액권 UI 구현 및 api 연동 [#18][#38] (#55)
    * feat: add LoginScreen file
    
    * feat: add LoginChoiceScreen, LoginScreen, and SignUpScreen file
    
    * feat: add logo image
    
    * feat: add LoginTextbox and update KkButton
    
    * feat: LoginScreen UI
    
    * refactor: clean up code
    
    * refactor: clean up code
    
    * style: enhance visual styling in KkButton
    
    * feat: LoginChoichScreen UI
    
    * style: update styles
    
    * style: update LoginTextbox styles
    
    * style: label fontweight 수정
    
    * fix: 린트 오류 수정
    
    * docs: 파일 이름 수정
    
    * style: variant 수정, enabled, error 타입 추가
    
    * feat: 회원가입 UI 개발 중
    
    * feat: 헤더 그림자 off 옵션 추가
    
    * feat: 약관동의 & 회원가입 UI
    
    * feat: 회원가입 UI
    
    * feat: 텍스트박스 컴포넌트 width, height props 추가
    
    * style: store 텍스트 컴포넌트 수정
    
    * style: 로그인 선택 버튼 그림자 추가
    
    * style: 로그 인 선택 버튼 그림자 추가
    
    * feat: main UI
    
    * feat: main UI
    
    * style: 텍스 트컴포넌트 코드 정리
    
    * docs: KkLoginTextbox.tsx -> KkTextbox.tsx
    
    * fix: 비밀번호 일치 문구 수정
    
    * fix: 문자열 분할 로직 수정
    
    * feat: main UI
    
    * fix: back 네비게이션
    
    * fix: KkTextbox로 변경
    
    * feat: 회원가입 유효성 검사
    
    * feat: 유효성 검사
    
    * fix: 회원가입 유효성 검사
    
    * style: KkTextbox, KkButton 스타일 수정에 따른 전반적인 코드 수정
    
    * chore: svg 버전 수정
    
    * fix: 폼 제출 버튼 검증 상태 추가
    
    * style: 인증하기 텍스트 변경
    
    * style: 스타일 코드 분리
    
    * style: 검색바 수정
    
    * style: main에 QR 버튼 추가
    
    * fix: 인증하기 검증 상태 추가
    
    * feat: 내 금액권 UI
    
    * .
    
    * fix: 내 금액권 수정 및 마이페이지 경로 추가
    
    * style: 배경 색 제거
    
    * style: main 가게 카드 패딩 수정
    
    * style: 스타일 코드 분리
    
    * style: unused 상태 스타일 추가
    
    * style: 검색 바 반응형 구현
    
    * feat: 내 금액권 구현 및 api연동
    
    * fix: 패키지 삭제
    
    * fix: status 값 변경
    
    * fix: 패키지 수정
    
    * style:scrollview 수정
    
    * Remove .env from tracking
    
    * feat: 반환 타입 명시 및 API 응답 제네릭 도입
    
    * feat: 코드 수정
    
    * feat: TanStack Query 적용
    
    * style: style 조정
    
    * style: main 화면 스타일 변경
    
    * fix: 표기 일관성 통일
    
    * docs: 파일명 변경
    
    * docs: 파일명 변경
    
    * feat: 로그아웃 알림창
    
    * feat: 마이페이지 section 추가
    
    * feat: 필드 업데이트
    
    * feat: 에러 상태 처리
    
    * style: 헤더 위치 변경
    
    * style: 내 금액권 스타일 변경
    
    * style: 금액권 페이지 헤더 그림자 변경
    
    * style: 사용 완료 탭 흐림처리
    
    * scroll view 제거
    
    * feat: UI 수정
    
    * feat: 사용완료 탭 라우팅 해제

--- src/screens/GiftCard/GiftCardDetail.styles.ts
+++ src/screens/GiftCard/GiftCardDetail.styles.ts
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.light.white,
-    paddingBottom: 100,
+    overflow: 'hidden',
   },
   headerContainer: {
     backgroundColor: colors.light.white,
