e1ecfc35cb456350325115b4080a7cab0a51d9cb
rladmsgki <rladmsgki@naver.com>
2025-08-19 22:14:46 +0900

    style: 테두리 추가

--- src/screens/paymenthistory/PaymentHistoryCard.tsx
+++ src/screens/paymenthistory/PaymentHistoryCard.tsx
@@ -23,6 +23,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     elevation: 3,
+    borderWidth: 1,
+    borderColor: colors.light.gray1,
   },
   dateText: {
     fontSize: 10,
