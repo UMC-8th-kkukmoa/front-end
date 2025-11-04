39401a4941f304dd51ddddfe0b5bc24c91f68b5f
chae1125 <y72682685@gmail.com>
2025-11-05 01:39:24 +0900

    fix: 중복되는 스타일 제거

--- src/screens/owner/OwnerQRScan/OwnerQRScan.tsx
+++ src/screens/owner/OwnerQRScan/OwnerQRScan.tsx
@@ -72,7 +72,6 @@ const styles = StyleSheet.create({
   bottomLeft: { bottom: 0, left: 0, borderLeftWidth: 10, borderBottomWidth: 10 },
   bottomRight: { bottom: 0, right: 0, borderRightWidth: 10, borderBottomWidth: 10 },
   processingText: { color: '#ffaa00', fontSize: 16, fontFamily: 'Pretendard-Bold' },
-  processingText: { color: '#ffaa00', fontSize: 16, fontFamily: 'Pretendard-Bold' },
 });
 
 export default function QrScannerScreen() {
